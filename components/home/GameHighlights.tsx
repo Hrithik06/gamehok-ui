c463651445196f5db9476dee3befc533eea74bd8
Hrithik06 <el99el03@gmail.com>
2026-04-29 23:21:39 +0530

    feat: implement GameHighlights component layout and add TODO for mobile bird

--- components/home/GameHighlights.tsx
+++ components/home/GameHighlights.tsx
@@ -1,3 +1,10 @@
 export default function GameHighlights() {
-  return <div>GameHighlights</div>;
+  return (
+    <section className="mb-8">
+      <div className="flex justify-between items-center my-4">
+        <h2 className="text-lg lg:text-xl ">Game Highlights </h2>
+        <p className="text-link text-xs lg:text-sm">VIEW ALL</p>
+      </div>
+    </section>
+  );
 }
